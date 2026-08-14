4b03ef1af950da4ad80e8dcce4197db7a9b0b34a
Elusivedamir <300641812+Elusivedamir@users.noreply.github.com>
2026-08-14 17:12:47 +0300

    fix: align AndroidX Core with compileSdk 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,7 +63,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.core:core-ktx:1.19.0")
+    implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.10.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.10.0")
     implementation("androidx.navigation:navigation-compose:2.9.8")
